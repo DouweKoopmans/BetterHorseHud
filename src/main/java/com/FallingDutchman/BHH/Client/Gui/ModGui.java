1e14d17adb9b754ef2baa873d4cdec496ff2f4d7
ItzSkillz <falling.dutchman@gmail.com>
2014-08-05 15:27:13 +0200

    cleaned up some unnecessary code

--- src/main/java/com/FallingDutchman/BHH/Client/Gui/ModGui.java
+++ src/main/java/com/FallingDutchman/BHH/Client/Gui/ModGui.java
@@ -3,21 +3,24 @@ package com.FallingDutchman.BHH.Client.Gui;
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.entity.Entity;
 import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
+
 
 //
 // GuiBuffBar implements a simple status bar at the top of the screen which
 // shows the current buffs/debuffs applied to the character.
 //
-public class ModGui extends GuiIngameForge
+public class ModGui extends Gui
 {
     private Minecraft mc;
 
     public ModGui(Minecraft mc)
     {
-        super(mc);
+        super();
 
         this.mc = mc;
     }
@@ -29,11 +32,7 @@ public class ModGui extends GuiIngameForge
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public void onRenderMountHealth(RenderGameOverlayEvent event)
     {
-        if (this.mc.thePlayer.ridingEntity == null || event.type != ElementType.HEALTHMOUNT)
-        {
-            return;
-        }
-        if (this.mc.thePlayer.ridingEntity != null)
+        if(this.mc.thePlayer.ridingEntity != null)
         {
             GuiIngameForge.renderFood = true;
         }
