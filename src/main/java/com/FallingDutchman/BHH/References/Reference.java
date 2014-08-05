f30179f556de527f83aceb783300bef0fccde402
ItzSkillz <falling.dutchman@gmail.com>
2014-08-06 00:07:40 +0200

    changed the mod_id to make more sense

--- src/main/java/com/FallingDutchman/BHH/References/Reference.java
+++ src/main/java/com/FallingDutchman/BHH/References/Reference.java
@@ -2,7 +2,7 @@ package com.FallingDutchman.BHH.References;
 
 public abstract class Reference
 {
-	public static final String MOD_ID = "BHH";
+	public static final String MOD_ID = "BetterHorseHud";
 	public static final String MOD_NAME = "Better Horse Hud";
 	public static final String VERSION = "1.0";
     public static final String CLIENT_PROXY_CLASS = "com.FallingDutchman.BHH.proxy.ClientProxy";
