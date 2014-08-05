447d637d520f5f35d44906526b9dcc1c7b76dfda
ItzSkillz <falling.dutchman@gmail.com>
2014-08-05 19:46:36 +0200

    made some progress towards the jumpbar and experience feature.

--- src/main/java/com/FallingDutchman/BHH/Client/Gui/ModGui.java
+++ src/main/java/com/FallingDutchman/BHH/Client/Gui/ModGui.java
@@ -1,22 +1,23 @@
 package com.FallingDutchman.BHH.Client.Gui;
 
+import com.FallingDutchman.BHH.utility.LogHelper;
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.EntityHorse;
 import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
+import org.lwjgl.input.Keyboard;
 
 
-//
-// GuiBuffBar implements a simple status bar at the top of the screen which
-// shows the current buffs/debuffs applied to the character.
-//
 public class ModGui extends Gui
 {
     private Minecraft mc;
+    public Entity ridingEntity;
+    private boolean YesOrNo = false;
 
     public ModGui(Minecraft mc)
     {
@@ -25,16 +26,42 @@ public class ModGui extends Gui
         this.mc = mc;
     }
 
-    //
-    // This event is called by GuiIngameForge during each frame by
-    // GuiIngameForge.pre() and GuiIngameForce.post().
-    //
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public void onRenderMountHealth(RenderGameOverlayEvent event)
     {
+        if (event.type == ElementType.EXPERIENCE || event.type == ElementType.FOOD || event.type == ElementType.JUMPBAR )
+        {
+            YesOrNo = true;
+        }
+
+        if (!event.isCancelable() || !YesOrNo)return;
+
         if(this.mc.thePlayer.ridingEntity != null)
         {
             GuiIngameForge.renderFood = true;
+        }
+        if(this.mc.thePlayer.ridingEntity instanceof EntityHorse)
+        {
+            if (!Keyboard.isKeyDown(Keyboard.KEY_SPACE) && event.type == ElementType.JUMPBAR )
+            {
+                LogHelper.info("spacebar not held down and element type = jumpbar");
+                event.setCanceled(true);
+                GuiIngameForge.renderJumpBar = false;
+                GuiIngameForge.renderExperiance = true;
+            } else if(Keyboard.isKeyDown(Keyboard.KEY_SPACE))
+            {
+                LogHelper.info("Spacebar is held down");
+                if (event.type == ElementType.JUMPBAR)
+                {
+                    GuiIngameForge.renderJumpBar = true;
+                    LogHelper.info("JumpBar is rendered");
+                }
+            }
+            if (event.type == ElementType.EXPERIENCE)
+            {
+                event.setCanceled(false);
+                GuiIngameForge.renderExperiance = true;
+            }
         }
     }
 }
