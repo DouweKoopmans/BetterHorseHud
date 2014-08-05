58853c03641f1152513174ad8dc76c11eb9d3c71
ItzSkillz <falling.dutchman@gmail.com>
2014-08-05 22:14:51 +0200

    got rid of unnecessary/unlogical things

--- src/main/java/com/FallingDutchman/BHH/BetterHorseHud.java
+++ src/main/java/com/FallingDutchman/BHH/BetterHorseHud.java
@@ -2,17 +2,13 @@ package com.FallingDutchman.BHH;
 
 import com.FallingDutchman.BHH.Client.Gui.ModGui;
 import com.FallingDutchman.BHH.References.Reference;
-import com.FallingDutchman.BHH.proxy.CommonProxy;
-import com.FallingDutchman.BHH.proxy.IProxy;
-import com.FallingDutchman.BHH.utility.LogHelper;
+import com.FallingDutchman.BHH.proxy.ClientProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.client.GuiIngameForge;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class BetterHorseHud
@@ -29,17 +25,9 @@ public class BetterHorseHud
     @Mod.EventHandler
     public void Init(FMLInitializationEvent event)
     {
-        LogHelper.info("Init");
         proxy.registerRenderers();
     }
 
-    @Mod.EventHandler
-    public void PostInit(FMLPostInitializationEvent event)
-    {
-        LogHelper.info("post init");
-    }
-
-    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
-    public static CommonProxy proxy;
-
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS)
+    public static ClientProxy proxy;
 }
--- src/main/java/com/FallingDutchman/BHH/proxy/CommonProxy.java
+++
@@ -1,6 +0,0 @@
-package com.FallingDutchman.BHH.proxy;
-
-public abstract class CommonProxy implements IProxy
-{
-    public void registerRenderers() {}
-}
