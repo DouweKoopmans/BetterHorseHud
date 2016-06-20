0de01fe1ed7d3fd6de000e659a587a51b8defd1e
Douwe Koopmans <douwekoopmans@hotmail.com>
2016-06-20 13:16:54 +0200

    create one version for 1.8.0, 1.8.8 and 1.8.9

--- src/main/java/com/FallingDutchman/BHH/BetterHorseHud.java
+++ src/main/java/com/FallingDutchman/BHH/BetterHorseHud.java
@@ -5,18 +5,15 @@ import com.FallingDutchman.BHH.References.Reference;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.*;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
-public class BetterHorseHud
-{
-    @Mod.Instance
-    public static BetterHorseHud instance;
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION,
+        acceptedMinecraftVersions = Reference.MC_VERSIONS)
+public class BetterHorseHud {
 
     @EventHandler
-    public void PreInit(FMLPreInitializationEvent event)
-    {
+    public void PreInit(FMLPreInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new ModGui(Minecraft.getMinecraft()));
     }
 }
--- src/main/java/com/FallingDutchman/BHH/References/Reference.java
+++ src/main/java/com/FallingDutchman/BHH/References/Reference.java
@@ -5,4 +5,5 @@ public abstract class Reference
 	public static final String MOD_ID = "BetterHorseHud";
 	public static final String MOD_NAME = "Better Horse Hud";
 	public static final String VERSION = "1.0";
+	public static final String MC_VERSIONS = "[1.8,1.8.9]";
 }
