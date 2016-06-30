6d1a77fb9ccca5f73cd7fb1f571683c25d3f92bd
Douwe Koopmans <douwekoopmans@hotmail.com>
2016-06-30 15:53:37 +0200

    add compatibility for 1.10 and 1.10.2

--- src/main/java/com/FallingDutchman/BHH/References/Reference.java
+++ src/main/java/com/FallingDutchman/BHH/References/Reference.java
@@ -1,9 +1,10 @@
 package com.FallingDutchman.BHH.References;
 
-public abstract class Reference
-{
+public final class Reference {
 	public static final String MOD_ID = "BetterHorseHud";
 	public static final String MOD_NAME = "Better Horse Hud";
 	public static final String VERSION = "${modversion}";
 	public static final String MC_VERSIONS = "${mcversion}";
+
+	private Reference(){}
 }
